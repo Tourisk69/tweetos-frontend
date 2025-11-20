c7e247c217e8245fec4ffc62d54ab392422ed7db
Geoffrey <geoffrey.demos@gmail.com>
2025-11-20 17:02:22 +0100

    redux

--- components/Tweet.js
+++ components/Tweet.js
@@ -3,7 +3,19 @@ import styles from '../styles/Tweet.module.css';
 function Tweet() {
     return(
         <div className={styles.container}>
-        <h1> home</h1>
+            <div>
+                <div>
+                    <h1> home</h1>
+                </div>
+                <div>
+                    <input type="text" />
+                </div>
+                <div>
+                    <p>longueur du tweet</p>
+                    <button>Tweet</button>
+                </div>
+            </div>
+        
         </div>
 
     );
