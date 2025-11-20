6680b20271838168c2597148854e3a5e17ee7d75
kalifa <kalifa.seydi@live.fr>
2025-11-20 17:15:36 +0100

    store

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,33 @@
 import '../styles/globals.css';
 import Head from 'next/head';
 
+// redux imports
+import { Provider } from 'react-redux';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import bookmarks from '../reducers/bookmarks';
+import user from '../reducers/user';
+import  hiddenArticles  from '../reducers/hiddenArticles';
+
+// redux-persist imports
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import storage from 'redux-persist/lib/storage';
+
+
+const reducers = combineReducers({ tweet, user});
+const persistConfig = { key: 'tweetos', storage };
+
+const store = configureStore({
+  reducer: persistReducer(persistConfig, reducers),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
+});
+
+
+
+const persistor = persistStore(store);
+
+
+
 function App({ Component, pageProps }) {
   return (
     <>
--- reducers/user.js
+++ reducers/user.js
@@ -5,7 +5,7 @@ const initialState = {
 }
 
 export const userSlice = createSlice({
-    name: 'user', 
+    name: 'users', 
     initialState,
     reducers: {
         login: (state, action) => {
